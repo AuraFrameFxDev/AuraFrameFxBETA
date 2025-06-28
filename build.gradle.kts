40703910e7c7fbb812a1b042137ad98ca1b82418
AuraKai <wehttam1989@gmail.com>
2025-06-28 17:55:01 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-// Top-level build file.
-// Plugin versions are managed in settings.gradle.kts within the pluginManagement block,
-// which refers to gradle/libs.versions.toml.
-
-@Suppress("UnstableApiUsage") // For using libs from version catalog in plugins block
 plugins {
     id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") apply false
