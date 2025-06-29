85047623cb8ad8a5b809fb559bc26df2588274b2
AuraKai <wehttam1989@gmail.com>
2025-06-29 02:28:34 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,18 @@
+
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-        // maven { url = uri("https://androidx.dev/storage/compose-compiler/repository/") } // For K2 Compose Compiler -- REMOVED
-        // maven { url = uri("https://plugins.gradle.org/m2/") } // This was for testing, gradlePluginPortal() is preferred
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://androidx.dev/storage/compose-compiler/repository/") }
     }
-    
-    plugins {
-        id("com.android.application") version "8.4.1"
-        id("org.jetbrains.kotlin.android") version "2.0.0"
-        id("com.google.dagger.hilt.android") version "2.56.2"
-        id("com.google.devtools.ksp") version "2.0.0-1.0.21"
-        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
-        id("org.openapitools.generator") version "7.6.0"
-        id("com.google.gms.google-services") version "4.4.2"
-        id("com.google.firebase.crashlytics") version "2.9.9"
-        id("com.google.firebase.firebase-perf") version "1.4.2"
-        // org.jetbrains.kotlin.plugin.compose is intentionally omitted, relying on Kotlin 2.0 + buildFeatures { compose = true }
+}
 
-    }
+plugins {
+    id("com.android.application") version "8.11.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" apply false
 }
 
 dependencyResolutionManagement {
@@ -33,4 +26,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AuraFrameFX"
 
-include:("app")
+include(":app")
