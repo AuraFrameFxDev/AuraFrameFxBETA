a337f29ccec2a67cb476994e53a6faacef8ad4e6
AuraKai <wehttam1989@gmail.com>
2025-06-29 16:15:01 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,18 +4,32 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.4.0" apply false
+        id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+        id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
+        id("com.google.dagger.hilt.android") version "2.56.2" apply false
+        id("com.google.gms.google-services") version "4.4.3" apply false
+        id("com.google.firebase.crashlytics") version "3.0.4" apply false
+        id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+        id("org.openapi.generator") version "7.6.0" apply false
+    }
 }
-
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
 }
-
-// If you have multiple modules, list them here. For example:
-include(":app")
-// include(":module1")
-// include(":module2")
 rootProject.name = "AuraFrameFxBETA"
+include(":app")
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
