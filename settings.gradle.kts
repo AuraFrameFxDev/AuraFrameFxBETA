548255b37e10752b294ee18fe07b16336387f85b
AuraKai <wehttam1989@gmail.com>
2025-06-29 16:32:29 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,12 +22,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
-}
+
 rootProject.name = "AuraFrameFxBETA"
 include(":app")
 
