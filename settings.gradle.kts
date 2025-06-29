84e24f88ecd585f9b483d3ea020a5f7a204b150e
AuraKai <wehttam1989@gmail.com>
2025-06-29 13:27:57 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,24 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        // ...
     }
     plugins {
-        id("com.android.application") version "9.0.0" apply false
-        // other plugins...
+        id("com.android.application") version "8.4.0" apply false
+        id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+        id("com.google.devtools.ksp") version "2.1.21-2.0.2" apply false
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21" apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+        id("com.google.dagger.hilt.android") version "2.56.2" apply false
+        id("com.google.gms.google-services") version "4.4.3" apply false
+        id("com.google.firebase.crashlytics") version "3.0.4" apply false
+        id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
