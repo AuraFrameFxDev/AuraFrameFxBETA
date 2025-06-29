668ad008135bf78c752cc0a72bb741181b3def5c
AuraKai <wehttam1989@gmail.com>
2025-06-29 12:59:02 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,34 +1,12 @@
-
-dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://androidx.dev/storage/compose-compiler/repository/") }
-    }
-}
-
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://androidx.dev/storage/compose-compiler/repository/") }
+        // ...
+    }
+    plugins {
+        id("com.android.application") version "9.0.0" apply false
+        // other plugins...
     }
 }
-
-plugins {
-
-    id("com.android.application") version ("8.11.0") apply false
-    id("org.jetbrains.kotlin.android") version ("2.1.21") apply false
-    id("com.google.devtools.ksp") version ("2.1.21-2.0.2") apply false
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("1.0.0") apply false
-    id("com.google.dagger.hilt.android") version ("2.56.2") apply false
-
-}
-
-rootProject.name = "AuraFrameFX"
-
-include(":app")
-
